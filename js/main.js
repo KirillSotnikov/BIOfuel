1bb6d28b016fa18d30eca85774e58ff678fd2542
Kyryll <sotnikov_k@outlook.com>
2018-08-08 15:37:42 +0300

    bio

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ $('.menu_image').click(function() {
 $('.close_btn').click(function() {
     $('.menu').removeClass('active');
 });
+
+$('[data-tab]').click(function() {
+
+	var target = $(this).attr('data-tab');
+	
+	$('.order-now_list a').removeClass('active');
+	$(this).addClass('active');
+
+	$('.order-now_container .order-now_grid').removeClass('active');
+	$('.order-now_container .order-now_grid' + target).addClass('active');
+
+});
+$('.call-button').click(function() {
+    $('.modal-container').addClass('active');
+});
+$('.up_close_arrow').click(function() {
+    $('.modal-container').removeClass('active');
+});
