9189c0b004b70b481ba5e841b7aa0c6ce53bd628
Kyryll <sotnikov_k@outlook.com>
2018-08-10 17:49:35 +0300

    bio

--- js/main.js
+++ js/main.js
@@ -15,10 +15,36 @@ $('[data-tab]').click(function() {
 	$('.order-now_container .order-now_grid').removeClass('active');
 	$('.order-now_container .order-now_grid' + target).addClass('active');
 
+
+	$('.tabulation_list_item').removeClass('active');
+	$(this).addClass('active');
+
+	$('.tabulation_content').removeClass('active');
+	$('.tabulation_content' + target).addClass('active');
 });
 $('.call-button').click(function() {
     $('.modal-container').addClass('active');
 });
 $('.up_close_arrow').click(function() {
     $('.modal-container').removeClass('active');
+});
+$('.slider.order-now_container').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	responsive: [
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 571,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ],
+	prevArrow: '<div class="arrow left"><img src="img/icons/left-arrow.svg"></div>',
+	nextArrow: '<div class="arrow right"><img src="img/icons/right-arrow.svg"></div>',
 });
